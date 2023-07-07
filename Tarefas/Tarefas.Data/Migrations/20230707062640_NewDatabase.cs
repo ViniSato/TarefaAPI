c08b6f2a8c15b3e007302c346fb9c70314a7b263
Vinícius Sato de Oliveira <vyniciussato@gmail.com>
2023-07-07 03:36:48 -0300

    Integração com a tabela de tarefas

---
+++ Tarefas/Tarefas.Data/Migrations/20230707062640_NewDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarefas.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Concluida = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Data = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefa", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefa");
+        }
+    }
+}
